9085f454dc5c90a9cadefd1ca45848d08b1e9293
Gothic <kalcifer_88@mail.ru>
2024-06-09 16:03:51 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,12 @@
+def get_maitrix(n,m,value):
+    matrix = []
+    for a in range(n):
+        b = []
+        matrix.append(b)
+        for c in range(m):
+            b.append(value)
+    print(matrix)
+    return matrix
+get_maitrix(2,2,10)
+get_maitrix(3,5,42)
+get_maitrix(4,2,13)
